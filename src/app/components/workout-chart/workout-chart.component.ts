00c32d60d45b00a925d7c9a607067b4a9e89c2a6
ayeshakz29 <ayeshafathima925@gmail.com>
2025-02-13 16:11:13 +0530

    Add files via upload

---
+++ src/app/components/workout-chart/workout-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-chart',
+  imports: [],
+  templateUrl: './workout-chart.component.html',
+  styleUrl: './workout-chart.component.css'
+})
+export class WorkoutChartComponent {
+
+}
